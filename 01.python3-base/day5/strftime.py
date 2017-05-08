f3bd8a1873c1efb8daf7d3a0f78869fdd86d44f3
felix_zh <chengfeng56@qq.com>
2017-05-08 11:08:51 +0800

    create

---
+++ 01.python3-base/day5/strftime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#Author:Felix_zh
+#E-mail:chengfeng56@qq.com
+#Function:
+import time
+
+x = time.localtime()
+# strftime 转换为 strptime 格式化字符窜
+print(time.strftime("%Y-%m-%d %H:%M:%S",x))
+
+# strptime 转换为 strftime
+print(time.strptime('2017-05-08 10:52:41',"%Y-%m-%d %H:%M:%S"))
--- 01.python3-base/day5/time_text.py
+++ 01.python3-base/day5/time_text.py
@@ -2,9 +2,16 @@
 # -*- coding: utf-8 -*-
 # Author:Felix_zh
 # E-mail:chengfeng56@qq.com
-# Function:
+# Function: 时间戳转换为格式化本地时间
+# gmtime：结果为UTC时区 localtime：结果为utc+8时区
 
 import time
 
-a = time.time()
-print(a)
+# 将时间戳转换为 strict_time(tuple)
+x = time.localtime() #获取time.localtime()
+print("x的方法有:\n",x)
+# print(help(x))
+print('今天是 %d年%d月%d日,%d时%d分%d秒' %(x.tm_year,x.tm_mon,x.tm_mday,x.tm_hour, x.tm_min, x.tm_sec))
+
+# 将strict_time(tuple)转换为时间戳 time.mktime
+print("将格式化时间转换为时间戳: " ,time.mktime(x),"秒")
