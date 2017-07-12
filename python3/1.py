ef3fb83f0fbc8ea1fc56dee5d28f6e4707628386
felix <chengfeng56@qq.com>
2017-07-12 21:41:11 +0800

    create

--- python3/1.py
+++ python3/1.py
@@ -3,3 +3,15 @@
 #Author:Felix_zh
 #E-mail:chengfeng56@qq.com
 #Function: first one
+
+import os
+import requests
+
+print(os.getcwd())
+print(os.getcwd())
+
+r=requests.get("https://www.baidu.com")
+
+print(r.url)
+print(r.encoding)
+print(r.text)
---
+++ python3/2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#Author:Felix_zh
+#E-mail:chengfeng56@qq.com
+#Function: 字符串 单，双引号 多行注释
+
+print("hello world!")
+print('hello world!')
+print('''
+This is the first line
+This is the second line
+Last line
+''')
+
+age = 3
+name = "Tom"
+print("{0} was {1} year old".format(name,age))
