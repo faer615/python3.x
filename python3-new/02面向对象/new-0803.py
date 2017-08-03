7ceebb3ca60256444e2412fc6fa8045cdf6ddb1e
felix-zh <chengfeng56@qq.com>
2017-08-03 17:10:49 +0800

    create

---
+++ python3-new/02面向对象/new-0803.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/3 17:10
+# @File    : new-0803.py
+# @Author  : felix
+# @Software: PyCharm
+# @E-mail  : chengfeng56@qq.com
+# @Function: 
